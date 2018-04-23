32bd74279e59d7c0b2ce754f98367f832336e74f
Henryk Kwiatek <szatanszmatan@gmail.com>
2018-04-23 14:43:10 +0200

    listening port as integer - test fix

--- guessitrest/test/test_bootstrap.py
+++ guessitrest/test/test_bootstrap.py
@@ -7,5 +7,5 @@ from guessitrest.bootstrap import bootstrap
 class TestBootstrap(object):
     def test_bootstrap(self, mocker):
         app = mocker.MagicMock()
-        bootstrap(app, ["-l", "192.168.0.1", "-p", "3333"])
-        app.run.assert_called_with(host='192.168.0.1', port='3333')
+        bootstrap(app, ["-l", "192.168.0.1", "-p", 3333])
+        app.run.assert_called_with(host='192.168.0.1', port=3333)
