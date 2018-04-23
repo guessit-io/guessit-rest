e67126c679282435cc1825c2c9d6cf88a7aa5329
Henryk Kwiatek <szatanszmatan@gmail.com>
2018-04-23 14:33:52 +0200

    listening port as integer

--- guessitrest/bootstrap.py
+++ guessitrest/bootstrap.py
@@ -9,7 +9,7 @@ def bootstrap(app, args=None):
 
     opts.add_argument('-l', '--listening-adress', dest='listening_adress', default='0.0.0.0',
                       help='Listening IP Adress of the HTTP Server.')
-    opts.add_argument('-p', '--listening-port', dest='listening_port', default=None,
+    opts.add_argument('-p', '--listening-port', dest='listening_port', default=None, type=int,
                       help='Listening TCP Port of the HTTP Server.')
 
     parsed_opts = opts.parse_args(args)
