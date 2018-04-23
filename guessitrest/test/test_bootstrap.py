7fa35ff127b22159d3a02926c158c6a3e4b52966
Henryk Kwiatek <szatanszmatan@gmail.com>
2018-04-23 14:53:20 +0200

    Allow integer for listening port option

--- guessitrest/test/test_bootstrap.py
+++ guessitrest/test/test_bootstrap.py
@@ -8,4 +8,4 @@ class TestBootstrap(object):
     def test_bootstrap(self, mocker):
         app = mocker.MagicMock()
         bootstrap(app, ["-l", "192.168.0.1", "-p", "3333"])
-        app.run.assert_called_with(host='192.168.0.1', port='3333')
+        app.run.assert_called_with(host='192.168.0.1', port=3333)
